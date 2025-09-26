2e42641c0acc8251c03dc28c4ce1c7fe2af2c82a
awayedogawa <away.edogawa@gmail.com>
2025-09-26 20:54:24 +0800

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAbCvNcwK7RDGKnV_Bw6aElF5tHGBNHM54",
+  authDomain: "ptk-2-c08f3.firebaseapp.com",
+  databaseURL: "https://ptk-2-c08f3-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "ptk-2-c08f3",
+  storageBucket: "ptk-2-c08f3.firebasestorage.app",
+  messagingSenderId: "702477103941",
+  appId: "1:702477103941:web:77a30352e32681abe2042c"
+};
